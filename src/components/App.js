b20e8858a64c24cd268123de2940e7923ee0833a
brianmujjuni <brianmujjuni@engineer.com>
2024-03-22 16:06:34 +0300

    add total points in the reducer function

--- src/components/App.js
+++ src/components/App.js
@@ -1,60 +1,73 @@
-import { useEffect, useReducer } from 'react'
-import Header from './Header'
-import Main from './Main'
-import Loader from './Loader'
-import Error from './Error'
-import StartScreen from './StartScreen'
-import Question from './Question'
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
 
-const initialState ={
+const initialState = {
   questions: [],
   //'loading' ,'error' 'ready','active','finished'
-  status: 'loading',
+  status: "loading",
   index: 0,
-  answer: null
-}
-function reducer(state,action){
-  switch(action.type){
-    case 'dataReceived':
-      return {...state,
-        questions: action.payload,
-        status: 'ready'}
-    case 'dataFailed':
-      return{...state,status: 'error'}
-    case 'start':
-      return {...state,status: 'active'}
-    case 'newNswer':
-      return {...state,answer: action.payload}
+  answer: null,
+  points: 0,
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
+    case "newNswer":
+      const question = state.question.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
     default:
-      return new Error('Action unknown')
-      
-      
-    
+      return new Error("Action unknown");
   }
-  
 }
 
-export default function App(){
-  const [{questions,status,index,answer}, dispatch] = useReducer(reducer,initialState)
-  const numQuestion = questions.length
+export default function App() {
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+  const numQuestion = questions.length;
 
-  useEffect(()=>{
-    fetch('http://localhost:8000/questions')
-    .then((res)=>res.json())
-    .then(data => dispatch({type: 'dataReceived',payload: data}))
-    .catch(err=> dispatch({type: 'dataFailed'}))
-  },[])
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
 
-return(
-  <div className='app'>
-   <Header/>
-   <Main>
-    {status === 'loading' && <Loader/>}
-    {status === 'error' && <Error/>}
-    {status === 'ready' && <StartScreen numQuestion={numQuestion} dispatch={dispatch}/>}
-    {status === 'active' && <Question question={questions[index]} 
-    dispatch={dispatch} answer={answer}/>}
-   </Main>
-  </div>
-)
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestion={numQuestion} dispatch={dispatch} />
+        )}
+        {status === "active" && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
+      </Main>
+    </div>
+  );
 }
